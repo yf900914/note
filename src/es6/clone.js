1f40b12a6cbf3981e27dbb7d57c90f0695c82b26
Lucien <etoahn@163.com>
2016-09-09 19:03:42 +0800

    clone

---
+++ src/es6/clone.js
@@ -0,0 +1,20 @@
+var clone=(obj)=>{
+    var buf;
+    if (obj instanceof Array){
+        buf=[];
+        for(i=0;i<obj.length;i++){
+            buf[i]=clone(obj[i]);
+        }
+        return buf;
+        }
+        else if(obj instanceof Object){
+             buf={};
+        for(k in obj){
+            buf[k]=clone(obj[k]);
+        }
+        return buf;
+    }
+    else{
+        return obj;
+    }
+}
