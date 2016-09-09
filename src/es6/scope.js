bbdde0e66dd21781bf634e7a1093ba4388c31aa5
Lucien <etoahn@163.com>
2016-09-09 15:55:33 +0800

    effect

--- src/es6/scope.js
+++ src/es6/scope.js
@@ -2,3 +2,15 @@
   let a = 10;
   var b = 1;
 }
+
+//a // ReferenceError: a is not defined.
+b // 1
+console.log(b);
+
+var func=()=>{
+    console.log("hello world!!!");
+}
+func();
+var a=1;
+a.a=2;
+console.log(a.a);
