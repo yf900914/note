3d973d20740f84d1391b4fedbc316f0144e14cbd
Lucien <etoahn@163.com>
2016-09-12 19:06:50 +0800

    randomint

--- src/es6/scope.js
+++ src/es6/scope.js
@@ -1,21 +1,22 @@
-{
-  let a = 10;
-  var b = 1;
-}
+// {
+//   let a = 10;
+//   var b = 1;
+// }
 
-//a // ReferenceError: a is not defined.
-b // 1
-console.log(b);
+// //a // ReferenceError: a is not defined.
+// b // 1
+// console.log(b);
 
-var func=()=>{
-    console.log("hello world!!!");
-}
-func();
+// var func=()=>{
+//     console.log("hello world!!!");
+// }
+// func();
+"use strict"
 var a=1;
 a.a=2;
 console.log(a.a);
 
-var randomint=(min,max)=>{
-  return Math.floor(Math.random()*(max-min+1)+min);
-}
-randomint(5,60);
+// var randomint=(min,max)=>{
+//   return Math.floor(Math.random()*(max-min+1)+min);
+// }
+// randomint(5,60);
